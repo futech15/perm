95fe40a3001b362ad01eabf27a357399f4e51c15
Rabar Sami <rabar.sami.ali@gmail.com>
2025-04-04 11:08:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ let state = {
     { month: 'May 2024', count: 12703, percentage: null }
   ],
   completedData: [
-    { date: '2024-03-25', count: 630, percentage: null },
-    { date: '2024-03-26', count: 662, percentage: null },
-    { date: '2024-03-27', count: 509, percentage: null },
-    { date: '2024-03-28', count: 467, percentage: null },
-    { date: '2024-03-29', count: 107, percentage: null },
+    { date: '2024-04-3', count: 610, percentage: null },
+    { date: '2024-04-2', count: 596, percentage: null },
+    { date: '2024-04-1', count: 605, percentage: null },
+    { date: '2024-03-31', count: 522, percentage: null },
     { date: '2024-03-30', count: 97, percentage: null },
-    { date: '2024-03-31', count: 522, percentage: null }
+    { date: '2024-03-29', count: 107, percentage: null },
+    { date: '2024-03-28', count: 467, percentage: null }
   ],
   todayCompleted: { count: 0, percentage: 0 } // Today's dynamic data
 };
